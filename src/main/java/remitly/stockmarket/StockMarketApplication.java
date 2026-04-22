e9876fda0bf8357f3d3195a0446ec107cc170931
Mikołaj Florczak <84631301+florczaq@users.noreply.github.com>
2026-04-22 14:40:07 +0200

    Add JavaDoc comments to StockMarketApplication class

--- src/main/java/remitly/stockmarket/StockMarketApplication.java
+++ src/main/java/remitly/stockmarket/StockMarketApplication.java
@@ -3,6 +3,12 @@ package remitly.stockmarket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Remitly Coding Task - Stock Market Application
+ * @author Mikołaj Florczak
+ * @version 1.0
+ * @since 21-04-2026
+ */
 @SpringBootApplication
 public class StockMarketApplication {
     
